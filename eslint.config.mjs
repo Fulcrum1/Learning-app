423d2ba24827b643671baaa7bbd65da5dce11d4c
guillaume trapet <guifall19@gmail.com>
2025-10-16 23:52:16 +0200

    Remove rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
@@ -20,13 +24,6 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
-  ...compat.config({
-    extends: ['next'],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  }),
 ];
 
 export default eslintConfig;
