2d7989dcab3ca4859a20ff272111e457de018818
Fulcrum1 <guifall19@gmail.com>
2025-10-22 11:11:19 +0200

    Change next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,61 @@
+// next.config.ts
 import type { NextConfig } from "next";
+import next from "next";
 
+const allowedOrigins = process.env.CORS_ORIGINS
+  ? JSON.parse(process.env.CORS_ORIGINS)
+  : ["http://localhost:3000", "http://localhost:8000"];
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  // swcMinify: true,
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? { exclude: ["error", "warn"] }
+        : false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: allowedOrigins.join(","),
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type,Authorization",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.brandfetch.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
-
 export default nextConfig;
