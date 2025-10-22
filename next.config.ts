9b256d47b3cc8a307b5f9f412cfb9f3209df402b
Fulcrum1 <guifall19@gmail.com>
2025-10-22 14:00:22 +0200

    Error deployement

--- next.config.ts
+++ next.config.ts
@@ -1,60 +1,7 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
-const allowedOrigins = process.env.CORS_ORIGINS
-  ? JSON.parse(process.env.CORS_ORIGINS)
-  : ["http://localhost:3000", "http://localhost:8000"];
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  // swcMinify: true,
-  compiler: {
-    removeConsole:
-      process.env.NODE_ENV === "production"
-        ? { exclude: ["error", "warn"] }
-        : false,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: allowedOrigins.join(","),
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type,Authorization",
-          },
-        ],
-      },
-    ];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.brandfetch.io",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-  redirects: async () => {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
+
 export default nextConfig;
