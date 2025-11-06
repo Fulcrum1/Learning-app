a6b6e57714660d54e3218dfb35cf7356442154ac
Fulcrum1 <guifall19@gmail.com>
2025-11-06 16:28:32 +0100

    Reset back

--- apps/api/_paths.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const moduleAlias = require('module-alias');
-
-moduleAlias.addAliases({
-  '@prisma': path.resolve(__dirname, 'src/prisma'),
-  '@app': path.resolve(__dirname, 'src'),
-});
