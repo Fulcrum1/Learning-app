4515072906b5234f6adf2974fad207ee4b262b83
Fulcrum1 <guifall19@gmail.com>
2025-11-13 11:00:34 +0100

    Error

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
+  // Configuration CORS
   app.enableCors({
-    origin: [process.env.APP_URL],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: process.env.APP_URL || 'https://learning-app-web-one.vercel.app', // URL par défaut si APP_URL n'est pas définie
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
-  app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(8000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT || 8000); // Utilisez PORT pour Vercel
 }
 bootstrap();
