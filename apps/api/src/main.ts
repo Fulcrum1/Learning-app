0ca1024a833111a5b5e5884d01807ae2edb8268c
Fulcrum1 <guifall19@gmail.com>
2025-12-01 16:35:39 +0100

    Frontend url from api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   // Enable CORS
   app.enableCors({
     // origin: [process.env.APP_URL || 'http://localhost:3000'],
-    origin: [process.env.APP_URL],
+    origin: process.env.APP_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
